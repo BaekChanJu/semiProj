3a93b36039660a90c1c01b2dc720f011ff6102f4
Baek Chan Ju <bcj0825@naver.com>
2022-11-16 19:40:46 +0900

    1116-2

---
+++ src/main/java/com/javassem/controller/BoardController.java
@@ -0,0 +1,32 @@
+package com.javassem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+
+	@RequestMapping("qnA")
+	public void qna() {
+		
+	}
+	
+	@RequestMapping("free")
+	public void free() {
+		
+	}
+	
+	@RequestMapping("styleReview")
+	public void styleReview() {
+		
+		
+	}
+	
+	@RequestMapping("write.do")
+	public void write () {
+		
+	}
+	
+	
+	
+}
